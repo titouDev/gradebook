be48a61372d77f7dcecda043efaeda8c986e6122
Cedric Bonnaffoux <cbonnaffoux@me.com>
2020-03-31 12:03:03 -0400

    list of doubles and foreach statement

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,10 +6,20 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            double[] numbers = new double[3] {12.5, 10.48, 20};
+            // same as                     
+            // var numbers = new[] {12.5, 10.48, 20};
+            double result = 0;
+
+            foreach(double number in numbers)
+            {
+                result += number;
+            }
+            System.Console.WriteLine(result);
+
+
             if(args.Length > 0)
             {
-                // 2 exemples de string concatenation
-                Console.WriteLine("Hello " + args[0] + "!");
                 Console.WriteLine($"Hello {args[0]}!");
             }
             else
